1e28201c0c800eefab9f6542e56d37bf0373cde5
Princy Rasoloarivony <prasoloarivony@gmail.com>
2024-10-22 10:22:15 +0200

    Amelioration site v4 et adaptation déploiement sur gcp

--- SublimPickAppFlask/main.py
+++ SublimPickAppFlask/main.py
@@ -3,7 +3,7 @@ import sys
 #sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.join(os.path.dirname(__file__), 'backend'))
 sys.path.append(os.path.join(os.path.dirname(__file__), 'backend', 'helpers'))
-
+sys.path.append(os.path.join(os.path.dirname(__file__), 'frontend'))
 from backend.app import app  # Importer l'application Flask depuis backend/app.py
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=False)
